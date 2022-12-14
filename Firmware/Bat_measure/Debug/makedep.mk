19c0149008f6deee29e887aaba30af84b1ee0276
Sprudlwassr <85758605+Sprudlwassr@users.noreply.github.com>
2022-12-14 17:08:23 +0100

    Add files via upload

---
+++ Firmware/Bat_measure/Debug/makedep.mk
@@ -0,0 +1,16 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+adc\adc.c
+
+lcd\lcd.c
+
+main.c
+
+oled\display.c
+
+oled\FontData.c
+
+oled\twi_soft.c
+
